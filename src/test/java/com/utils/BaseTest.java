f826930e4b30af9ef14e6772daf4c90d2aa3e9c8
Sakshi900 <sakshib9019@gmail.com>
2025-09-02 10:49:40 -0400

    annotations update

--- src/test/java/com/utils/BaseTest.java
+++ src/test/java/com/utils/BaseTest.java
@@ -3,19 +3,21 @@ package com.utils;
 import com.framework.config.TestConfig;
 import com.framework.driver.DriverFactory;
 import io.qameta.allure.Step;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeClass(alwaysRun = true)
     @Step("Initialize WebDriver and navigate to base URL")
     public void setUp() {
         DriverFactory.initDriver();
         DriverFactory.getDriver().get(TestConfig.baseUrl());
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterClass(alwaysRun = true)
     @Step("Quit WebDriver")
     public void tearDown() {
         DriverFactory.quitDriver();
